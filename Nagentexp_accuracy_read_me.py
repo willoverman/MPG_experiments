d840d329b6717e72889a6726aad4393f39a33f6e
Stef Leonardos <36231425+sl3onardos@users.noreply.github.com>
2021-04-29 18:25:26 +0300

    Some important changes
    After running experiments with Nagentexp_accuracy,  I found following issues:
    1. There are multiple v_star and we need to give all of them, because sometimes it converges to a different equilibrium. So, now, I hardcode all equilbria and take the minimum distance as current accuracy. This should be correct.
    2. Why is it converging to different equilibria (since before we got 200/200 to the same one)? I suppose, it is because I changed the Qfunction (to not evaluate value_function routine every time). So, in this file now, I reverted back.
    3. The last iteration, has frequently a negative performance around -2. That is, the performance starts from very bad and goes down (from 200, 150, 100 to 0) but the very last one is sometimes negative instead of exactly zero or slightly above zero. Since we compare with some v_star, this is not possible and puzzling.

---
+++ Nagentexp_accuracy_read_me.py
@@ -0,0 +1,159 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 2
+M = 2
+S = 2
+
+def projection_simplex_sort(v, z=1):
+    if v.sum() == z and np.alltrue(v >= 0):
+        # best projection: itself!
+        return v
+    n_features = v.shape[0]
+    u = np.sort(v)[::-1]
+    cssv = np.cumsum(u) - z
+    ind = np.arange(n_features) + 1
+    cond = u - cssv / ind > 0
+    rho = ind[cond][-1]
+    theta = cssv[cond][-1] / float(rho)
+    w = np.maximum(v - theta, 0)
+    return w
+
+def get_reward(state, acts):
+    if state == 0:
+        if acts[0]== 0 and acts[1] == 0:
+            return [5,2]
+        elif acts[0] == 0 and acts[1] == 1:
+            return [-1,-2]
+        elif acts[0] == 1 and acts[1] == 0:
+            return [-5,-4]
+        else:
+            return [1,4]
+
+    elif state == 1:
+        if acts[0] == 0 and acts[1] == 0:
+            return [32,29]
+        elif acts[0] == 0 and acts[1] == 1:
+            return [-28,-29]
+        elif acts[0] == 1 and acts[1] == 0:
+            return [-32,-31]
+        else:
+            return [28,31]
+    else:
+        return [0,0]
+
+def get_next_state(state, acts):
+    if acts[0] != acts[1]:
+        if state == 0:
+            return 1
+        return 0
+    return state
+
+def pick_action(prob_dist):
+    acts = [i for i in range(len(prob_dist))]
+    action = np.random.choice(acts, 1, p = prob_dist)
+    return action
+
+def visit_dist(state, policy, gamma, T):
+    visit_states = {st: np.zeros(T) for st in range(S)}        
+
+    for i in range(15):
+        curr_state = state
+        visit_states[curr_state][0] += 1
+        for t in range(1,T):
+            actions = [pick_action(policy[state, i]) for i in range(N)]
+            curr_state = get_next_state(curr_state, actions)
+            visit_states[curr_state][t] += 1
+    
+    # This is the un-normalized distribution. The normalizing constant would be (1-gamma^T)/(1-gamma) (or 1-gamma^{T+1}/1-gamma) depending
+    # on where the index ends. But according to the formula in Kakade, the normalizing constant appears in the derivative of the value function
+    # in which we are interested in, so it cancels out. We probably need to double-check this though.
+    dist = [np.dot(v/10,gamma**np.arange(T)) for (k,v) in visit_states.items()]
+    return dist 
+
+def value_function(policy, gamma, T):
+    repeat = 10
+    value_fun = {(s,i): 0 for s in range(S) for i in range(N)}
+    for state in range(S):
+        for k in range(repeat):
+            curr_state = state
+            for t in range(T):
+                actions = [pick_action(policy[curr_state, i]) for i in range(N)]
+                rewards = get_reward(curr_state, actions)
+                for i in range(N):
+                    value_fun[state,i] += (gamma**t)*rewards[i]
+                curr_state = get_next_state(curr_state, actions)
+    value_fun.update((x,v/repeat) for (x,v) in value_fun.items())
+    return value_fun
+
+def current_accuracy(mu,v_pi,v_star,gamma,T):
+    total_dif = N * [0]
+    for agent in range(N):
+        for s in range(S):
+            total_dif[agent] += mu[s]*(v_star[s,agent]-v_pi[s,agent])
+    return np.max(total_dif)
+
+def Q_function(agent, state, action, policy, gamma, T, samples):
+    tot_reward = 0
+    for i in range(samples):
+        actions = [pick_action(policy[state, i]) for i in range(N)]
+        actions[agent] = action
+        curr_state = state
+        reward = get_reward(state, actions)
+        curr_state = get_next_state(curr_state, actions)
+        tot_reward += reward[agent]
+        tot_reward += value_function(policy, gamma, T)[curr_state, agent]
+
+    return (tot_reward / samples)
+
+def policy_gradient(mu, max_iters, gamma, eta, T, samples, epsilon):
+    policy = {}
+    policy_star_1 = {(0, 0): np.array([1., 0.]), (0, 1): np.array([0., 1.]), (1, 0): np.array([0., 1.]), (1, 1): np.array([0., 1.])}
+    policy_star_2 = {(0, 0): np.array([1., 0.]), (0, 1): np.array([0., 1.]), (1, 0): np.array([1., 0.]), (1, 1): np.array([1., 0.])}
+    policy_star_3 = {(0, 0): np.array([0., 1.]), (0, 1): np.array([1., 0.]), (1, 0): np.array([0., 1.]), (1, 1): np.array([0., 1.])}
+    policy_star_4 = {(0, 0): np.array([0., 1.]), (0, 1): np.array([1., 0.]), (1, 0): np.array([1., 0.]), (1, 1): np.array([1., 0.])}
+    v_star_1 = value_function(policy_star_1,gamma,T)
+    v_star_2 = value_function(policy_star_2,gamma,T)    
+    v_star_3 = value_function(policy_star_3,gamma,T)
+    v_star_4 = value_function(policy_star_4,gamma,T)    
+
+    ca = []
+    
+    for s in range(S):
+        for i in range(N):
+            policy[s, i] = [1/M for i in range(M)]
+            
+    for t in range(max_iters):
+
+        a_dist = M *[[0]] #i didnt know what to call it, just intermediate dist values
+        for st in range(S):
+            a_dist[st] = visit_dist(st, policy, gamma, T)
+        
+        b_dist = M * [0] #the ones we actually use
+        for st in range(S):
+            b_dist[st] = np.dot(a_dist[st], mu)
+
+        v_pi = value_function(policy,gamma,T)
+        m_ca = min(current_accuracy(mu,v_pi,v_star_1,gamma,T),current_accuracy(mu,v_pi,v_star_2,gamma,T),current_accuracy(mu,v_pi,v_star_3,gamma,T),current_accuracy(mu,v_pi,v_star_4,gamma,T))
+        ca.append(m_ca)
+        if ca[t] < epsilon:
+            iter = t
+            break
+        else:
+            iter = -100
+        
+        grads = np.zeros((N, S, M))
+        for agent in range(N):
+            for st in range(S):
+                for act in range(M):
+                    grads[agent, st, act] = b_dist[st] * Q_function(agent, st, act, policy, gamma, T, samples)
+
+        for agent in range(N):
+            for st in range(S):
+                policy[st, agent] = projection_simplex_sort(np.add(policy[st, agent], eta * grads[agent,st]), z=1)
+        
+#         print(policy)
+        
+    return policy,iter,ca
+
+policy_gradient([1, 0],60,0.99,0.001,10,5,0.01)
